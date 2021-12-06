c96cb796ee026fa2cb10d65bec46443f8d6b3ea6
Arslan Abdullaev <ars352@yahoo.com>
2021-12-06 22:54:25 +0300

    create app

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldLabel.isHidden = true
+        startButton.layer.cornerRadius = 10
+        helloWorldLabel.textColor = .systemMint
     }
 
-
+    @IBAction func startPressed() {
+        helloWorldLabel.isHidden.toggle()
+        if helloWorldLabel.isHidden {
+            startButton.setTitle("Show text", for: .normal)
+        } else {
+            startButton.setTitle("Hide text", for: .normal)
+        }
+    }
 }
 
